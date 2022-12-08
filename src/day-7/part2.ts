9ddda2073cbdd6ac9e9be2db707b008950453c94
JBoot1 <jboot@dunelm.com>
2022-12-08 15:50:05 +0000

    day 7 part 2

---
+++ src/day-7/part2.ts
@@ -0,0 +1,107 @@
+import { readFileSync } from 'fs'
+import path from 'path'
+
+const data = readFileSync(path.resolve(__dirname, './input.txt')).toString()
+const commands = data.split('\n')
+
+type Directory = {
+  name: string
+  directFileSizes: number
+  totalSize: number
+  subDirectories: Directory[]
+  parentDirectory: Directory | null
+}
+
+const home: Directory = {
+  name: '/',
+  directFileSizes: 0,
+  totalSize: 0,
+  subDirectories: [],
+  parentDirectory: null
+}
+
+let currentDir = home
+
+commands.forEach((command, index) => {
+  if(command.includes('$ cd')) {
+    const portions = command.split(' ')
+
+    if(portions[2] === '..') { // go up one directory
+      if (currentDir.parentDirectory) {
+        currentDir = currentDir.parentDirectory
+      }
+    }
+    else { // moving to subdirectory
+      if (portions[2] === '/') {
+        currentDir = home
+      }
+      else { // check subdirectory exists, if not, create subdirectory entry and move to
+        const nextDir = currentDir.subDirectories.find(directory => directory.name === portions[2])
+
+        if (nextDir) {
+          currentDir = nextDir
+        }
+        else {
+          const newDir: Directory = {
+            name: portions[2],
+            directFileSizes: 0,
+            totalSize: 0,
+            subDirectories: [],
+            parentDirectory: currentDir
+          }
+          currentDir.subDirectories.push(newDir)
+          currentDir = newDir
+        }
+      }
+    }
+  }
+  else if (command.includes('dir ')) { // check directory listed exists, if not, create subdirectory entry
+    const directoryName = command.split(' ')[1]
+
+    const directory = currentDir.subDirectories.find(directory => directory.name === directoryName)
+
+    if (!directory) {
+      currentDir.subDirectories.push({
+        name: directoryName,
+        directFileSizes: 0,
+        totalSize: 0,
+        subDirectories: [],
+        parentDirectory: currentDir
+      })
+    }
+  }
+  else if (command.includes('$ ls')) {
+    // nothing to process
+  }
+  else { // must be a file, record filesize
+    const filesize = command.split(' ')[0]
+    currentDir.directFileSizes += parseInt(filesize)
+  }
+})
+
+const setTotalDirectorySize = (directory: Directory): number => {
+  const subDirectorySizes = directory.subDirectories.map(subDirectory => setTotalDirectorySize(subDirectory))
+  
+  const subDirectoryTotal = subDirectorySizes.reduce((prev, curr) => prev + curr, 0)
+
+  directory.totalSize = directory.directFileSizes + subDirectoryTotal
+
+  return directory.totalSize
+}
+
+setTotalDirectorySize(home)
+
+let directoryToDelete = home
+
+const spaceRequired = 30000000 - (70000000 - home.totalSize)
+
+const getSmallestEnoughDirectory = (directory: Directory) => {
+  if (directory.totalSize >= spaceRequired && directory.totalSize < directoryToDelete.totalSize) {
+    directoryToDelete = directory
+  }
+  directory.subDirectories.forEach(dir => getSmallestEnoughDirectory(dir))
+}
+
+getSmallestEnoughDirectory(home)
+
+console.log(directoryToDelete.totalSize)
